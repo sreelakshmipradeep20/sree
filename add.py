dce5b4f63c975dbe20881a07195bfcf1d0ec1eff
SREELAKSHMI PRADEEP <50716@fisat>
2018-04-18 01:19:04 -0400

    create a function to add 4 numbers

--- add.py
+++ add.py
@@ -1,5 +1,7 @@
 x=int(raw_input("enter the first number:"))
 y=int(raw_input("enter the second number:"))
 z=int(raw_input("enter the third number:"))
-sum=x+y+z
+w=int(raw_input("enter the fourth number:"))
+sum=x+y+z+w
 print sum
+
